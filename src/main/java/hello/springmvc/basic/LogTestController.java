7c8b1d0bad158ce065eb9d65e26fc9cfe46386d6
kk210001 <kk210001@naver.com>
2022-07-02 21:13:04 +0900

    Initial commit

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,29 @@
+package hello.springmvc.basic;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller view 반환
+@RestController // 메시지바디 데이터 반환, restAPI에서 사용
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+        log.info(" info log={}", name);
+
+        return "ok";
+    }
+}
