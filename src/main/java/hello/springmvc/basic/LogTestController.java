115e1a7aa91b668b464cfde5108103edd39bb8db
kk210001 <kk210001@naver.com>
2022-07-02 21:23:46 +0900

    @Controller는 반환 값이 String 이면 view 이름으로 인식 @RestController는 http 메시지 바디에 바로 입력하여 String 정상적으로 반환 가능
    로그 출력(Slf4j)
    시간, 로그 레벨, 프로세스id, 쓰레드명,  클래스명, 로그 메시지 순
    로그레벨 default : info ~ error 최상위 : trace

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,6 +1,7 @@
 package hello.springmvc.basic;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -8,21 +9,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@Controller view 반환
+@Slf4j
 @RestController // 메시지바디 데이터 반환, restAPI에서 사용
 public class LogTestController {
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    
+//    private final Logger log = LoggerFactory.getLogger(getClass()); -> @Slf4j
 
     @RequestMapping("/log-test")
     public String logTest() {
         String name = "spring";
 
         System.out.println("name = " + name);
-
+        log.info(" info log=" + name);//info log=spring 다음 info 실행, 비권장
+        
+        
         log.trace("trace log={}", name);
         log.debug("debug log={}", name);
         log.warn(" warn log={}", name);
         log.error("error log={}", name);
-        log.info(" info log={}", name);
+        log.info(" info log={}", name);// 파라미터만 보내서 info 실행
 
         return "ok";
     }
