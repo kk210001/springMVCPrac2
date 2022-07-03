7af20f521bb19d23d2073a47b5a95a29145da14f
kk210001 <kk210001@naver.com>
2022-07-03 20:03:47 +0900

    @RequestMapping("/mapping/users/{userId}/orders/{orderId}") {userId}, {orderId} -> 경로 번수 @RequestMapping 다양한 param headers 특정 header 없으면 오류 params 특정 param 없으면 오류 consumes 특정 미디어 타입일 경우만 호출 produces 특정 http 요청일 경우만 호출

---
+++ src/main/java/hello/springmvc/basic/RequestMapping/MappingController.java
@@ -0,0 +1,62 @@
+package hello.springmvc.basic.RequestMapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping(value = {"/hello-basic", "/hello-go"})
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+
+    @GetMapping("/mapping/{userId}") //url 경로 템플릿화 할수 있음 경로 변수 ->{userId}
+//    public String mappingPath(@PathVariable String userId) {
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}") //다중 경로 변수
+    public String mappingPath(@PathVariable String userId, @PathVariable Long
+            orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    //특정 param 없으면 오류
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+    //특정 header 없으면 오류
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+    //특정 미디어 타입일 경우에만 호출 content-type
+//    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    //http 요청 accept, produce accept
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
